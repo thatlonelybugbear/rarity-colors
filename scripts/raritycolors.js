c86c77ebad8a90357076bcc2957366cd9adbaa3e
kandashi <jacobmpalmer@live.co.uk>
2021-11-09 14:37:44 +0000

    2nd

--- scripts/raritycolors.js
+++ scripts/raritycolors.js
@@ -1,8 +1,22 @@
+CONFIG.debug.hooks = true
 Hooks.on('renderActorSheet', (actor, html) => {
     let items = html.find($(".items-list .item"))
-    for(let i of items){
+    for(let i of items) {
         let id = i.outerHTML.match(/data-item-id="(.*?)"/)
         let rarity = actor.object.items.get(id[1]).data.data.rarity
         if(rarity !== "" && rarity !== undefined) i.classList.add(rarity.slugify().toLowerCase())
     }
 });
+
+Hooks.on("renderSidebarTab", (bar, html) => {
+    let items = html.find(".directory-item.entity.item")
+    for(let i of items) {
+        let id = i.outerHTML.match(/data-entity-id="(.*?)"/)
+        let item = game.items.get(id[1])
+        let rarity = item.data.data.rarity
+        let type = item.data.type
+        if(rarity !== "" && rarity !== undefined) i.classList.add(rarity.slugify().toLowerCase())
+        if(type === "spell") i.classList.add("spell")
+        if(type === "feat") i.classList.add("feat")
+    }
+})
