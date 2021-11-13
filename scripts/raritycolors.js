d577cea727d3b9fb163c6c49fbc8b52c7a3223d0
kandashi <jacobmpalmer@live.co.uk>
2021-11-13 11:16:50 +0000

    Update raritycolors.js

--- scripts/raritycolors.js
+++ scripts/raritycolors.js
@@ -3,7 +3,8 @@ Hooks.on('renderActorSheet', (actor, html) => {
     let items = html.find($(".items-list .item"))
     for(let i of items) {
         let id = i.outerHTML.match(/data-item-id="(.*?)"/)
-        let rarity = actor.object.items.get(id[1]).data.data.rarity
+        if(!id) return
+        let rarity = actor.object.items.get(id[1]).data.data?.rarity
         if(rarity !== "" && rarity !== undefined) i.classList.add(rarity.slugify().toLowerCase())
     }
 });
@@ -15,6 +16,7 @@ Hooks.on("renderSidebarTab", (bar, html) => {
     let items = html.find(".directory-item.entity.item")
     for(let i of items) {
         let id = i.outerHTML.match(/data-entity-id="(.*?)"/)
+        if(!id) return
         let item = game.items.get(id[1])
         let rarity = item.data.data.rarity
         let type = item.data.type
